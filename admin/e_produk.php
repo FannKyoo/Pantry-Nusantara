e1f252cd926947c811a83f3d297049dd78d9a1cc
FannKyoo <marfancepu@gmail.com>
2025-05-20 21:47:36 +0700

    tes commit

--- admin/e_produk.php
+++ admin/e_produk.php
@@ -79,7 +79,7 @@ if (isset($_POST['update'])) {
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>Produk - PantryNusantara Admin</title>
+    <title>Produk - FannKyoo Admin</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
