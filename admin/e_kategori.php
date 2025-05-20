c95ba3af6d602547c5e61444d58803fb5835d447
FannKyoo <marfancepu@gmail.com>
2025-05-20 21:56:52 +0700

    tes lagi

--- admin/e_kategori.php
+++ admin/e_kategori.php
@@ -26,7 +26,7 @@ if(isset($_POST['simpan'])) {
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Kategori Produk - FannKyoo Admin</title>
+  <title>Kategori Produk - PantryNusantara Admin</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
